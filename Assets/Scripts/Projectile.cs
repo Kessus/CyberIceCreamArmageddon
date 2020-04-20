af0c5cc8512c8dbdb6f89cd176827652193fe3e9
Weronika Wolny <wewolny@users.noreply.github.com>
2020-04-20 17:59:07 +0200

    Projectile movement

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    public int damage = 40;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb.velocity = transform.right * speed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D hitInfo)
     {
-        
+        // test target
+        Debug.Log(hitInfo.name);
+
+        // damage - implement enemy!!
+
+        /*Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        */
+         
+        Destroy(gameObject);
     }
+
 }
