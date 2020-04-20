17d35b3f7ab5b43159f216e8c8883b519ccaf378
Kazu <grafiszek@wp.pl>
2020-04-20 17:56:55 +0200

    Weapon behaviour and shooting scripts

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public WeaponBehaviour weaponBehaviour;
+    public string triggerKey = "Fire1";
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown(triggerKey))
+        {
+            weaponBehaviour.Execute();
+        }
+    }
+}
