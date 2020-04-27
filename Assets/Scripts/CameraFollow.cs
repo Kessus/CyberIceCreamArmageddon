1513397f5719f93c94232a7846bb7ace8836bbc0
Kazu <grafiszek@wp.pl>
2020-04-27 14:36:27 +0200

    Camera movement restriction

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform player;
+    [Tooltip("Min and Max of camera X transform value")]
+    public Vector2 xBoundaries;
+    [Tooltip("Min and Max of camera Y transform value")]
+    public Vector2 yBoundaries;
 
     private void Update()
     {
         if(player != null)
-            transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+            transform.position = new Vector3(Mathf.Clamp(player.position.x, xBoundaries.x, xBoundaries.y), Mathf.Clamp(player.position.y, yBoundaries.x, yBoundaries.y), transform.position.z);
     }
 }
